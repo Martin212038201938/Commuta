4d0b0fd003a066c9e9b4958bf12c17b7267b87b2
Martin212038201938 <martin@yellow-boat.com>
2025-10-08 09:23:39 +0200

    Add CommutaApp main file

---
+++ CommutaApp.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import CoreLocation
+
+@main
+struct CommutaApp: App {
+    @StateObject private var locationManager = LocationManager()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(locationManager)
+        }
+    }
+}
+
+// Standortmanager für die App
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation?
+    @Published var locationStatus: CLAuthorizationStatus?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.location = location
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationStatus = status
+    }
+}
